945632e6f0ba8c9275c61cd68c9fabe93a578d64
Biagio Licari <biagiolicari7@gmail.com>
2021-03-17 10:03:51 +0000

    finish

--- Main.java
+++ Main.java
@@ -1,39 +1,38 @@
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-        while (true) {
-            Scanner inputScanner = new Scanner(System.in);
-            System.out.println("\nInsert the word : ");
-            String word = inputScanner.nextLine();
-            if (word.isEmpty() || !word.matches("[A-Za-z]+")) {
-                System.out.println("Please insert a valid word");
-                continue;
-            }
-            MultiSetPermutation maxNumOfPermutations = new MultiSetPermutation(word);
-            maxNumOfPermutations.calculateFactorial();
-            if (maxNumOfPermutations.getMaxNumberOfPermutation() < 0) {
-                System.out.println("\nOPS, I got an error. Sorry :(");
-                continue;
-            }
-            System.out.println("The maximum numbers of anagrams available for the word: " + word.toUpperCase() + " is: "
-                    + maxNumOfPermutations.getMaxNumberOfPermutation());
-
-            System.out.println(
-                    "Insert an integer in range [ " + 1 + ", " + maxNumOfPermutations.getMaxNumberOfPermutation() + " ] : ");
-            int numOfAnagrams = inputScanner.nextInt();
-            if (numOfAnagrams < 0) {
-                System.out.println("Please insert a valid integer :)");
-                continue;
-            } else {
-                if (numOfAnagrams == 0) {
-                    System.out.print("You inserted 0 as integer value, so I find for you all possible anagrams");
-                }
-                Anagram anagrams = new Anagram(word, numOfAnagrams);
-                anagrams.makeAnagrams(anagrams.getWordInLetter(), anagrams.getFixedLetter());
-            }
-        }
+  public static void main(String[] args) {
+    // write your code here
+    System.out.println("Insert the word : ");
+    Scanner inputScanner = new Scanner(System.in);
+    String word = inputScanner.nextLine();
+    inputScanner.close();
+    if (word.isEmpty() || !word.matches("[A-Za-z]+")) {
+      System.out.println("Please insert a valid word\n");
+      return;
     }
+    MathUtils mathUtils = new MathUtils(word);
+    int maxNumOfAnagrams = mathUtils.calculateFactorial();
+    if (maxNumOfAnagrams < 0) {
+      System.out.println("OPS, I got an error. Sorry :(");
+      return;
+    }
+    if (args.length > 0 && args[0] != null) {
+      int numOfAnagrams = Integer.parseInt(args[0]);
+      if (numOfAnagrams < 0) {
+        System.out.println("Please insert a valid integer via command line :)\n");
+        return;
+      }
+      if (numOfAnagrams == 0) {
+        System.out.print("You inserted 0 as integer value, so I find for you all possible anagrams\n");
+      }
+      System.out.println("Anagrams to print : " + numOfAnagrams + " of " + maxNumOfAnagrams + "\n");
+      Anagram anagrams = new Anagram(word, numOfAnagrams);
+      anagrams.makeAnagrams();
+      System.out.println("\n");
+    } else {
+      System.out.println("Insert an Integer via command line\n");
+      return;
+    }
+  }
 }
